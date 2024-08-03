a2e06567b6ea28794bc0d7c9038aae278e7bc178
Cesar Rodriguez <cesarodriguez4@gmail.com>
2024-08-03 17:32:50 -0400

    typewriter effect added

--- app/components/ReceiverMessage.tsx
+++ app/components/ReceiverMessage.tsx
@@ -1,9 +1,26 @@
+'use client';
+
+import Typewriter from 'typewriter-effect';
 export const ReceiverMessage = ({message}: {message: string}) =>
     (
        <div className="flex justify-end mr-12">
            <div className="flex flex-col items-end">
                <div className="flex items-center justify-end">
-                   <p className="p-4 bg-gray-100 rounded-lg dark:bg-neutral-800/30">{message}</p>
+                   <div className="p-4 bg-gray-100 rounded-lg dark:bg-neutral-800/30">
+                          <Typewriter
+                            options={
+                                {
+                                    loop: false,
+                                    delay: 10,
+                                }
+                            }
+                            onInit={(typewriter) => {
+                                typewriter.typeString(message)
+                                    .start();
+                            }
+                            }
+                          />
+                    </div>
                </div>
            </div>
        </div>

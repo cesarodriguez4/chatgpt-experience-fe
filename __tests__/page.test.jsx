83ce4b0ea862ecf79aa889632d54d77bdb4146a2
Cesar Rodriguez <cesarodriguez4@gmail.com>
2024-08-04 13:08:57 -0400

    Include basic tests using Jest

---
+++ __tests__/page.test.jsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+    const heading = screen.getByRole('heading', { level: 2 })
+    expect(heading).toHaveTextContent('ChatGPT Experience')
+    expect(heading).toBeInTheDocument()
+  })
+})
